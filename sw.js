5ff956dd12106eba7d2c3c58ef446e199cedb8f6
zhugaofeng-mf <zhugaofeng@megliofuturo.co.jp>
2026-02-23 23:32:59 +0900

    v4: futuristic dark UI, CSS mini-maps, senior mode, PWA offline support

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'narita-v4';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('translate.googleapis.com')) return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
